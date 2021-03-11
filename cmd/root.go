6679a2ceb1688c7fbb72d6cb637607085cd15d97
Robin Kearney <robin@kearney.co.uk>
2021-03-11 11:43:51 +0000

    much nicer now

--- cmd/root.go
+++ cmd/root.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
-	"path/filepath"
 	"strings"
 	"time"
 
@@ -23,11 +22,29 @@ var (
 	}
 
 	// Options
-	number int
+	number    int
+	types     []string
+	separator string
+)
+
+const (
+	dataFileSuffix = ".txt"
 )
 
 func init() {
+	allTypes := possibleTypes()
+
 	rootCmd.PersistentFlags().IntVarP(&number, "number", "n", 1, "Number of names to generate")
+	rootCmd.PersistentFlags().StringSliceVarP(&types, "types", "t", allTypes, "Types to include")
+	rootCmd.PersistentFlags().StringVarP(&separator, "separator", "s", "-", "Separator to use between words")
+}
+
+func possibleTypes() []string {
+	var names []string
+	for _, n := range data.AssetNames() {
+		names = append(names, strings.TrimSuffix(n, dataFileSuffix))
+	}
+	return names
 }
 
 // Execute is respomnsible for executing the viper command
@@ -35,31 +52,30 @@ func Execute() error {
 	return rootCmd.Execute()
 }
 
+// generate is the entry point into the name generator from the root Cobra cmd
 func generate(cmd *cobra.Command, args []string) {
-	perms, err := readData()
+	perms, err := readData(types)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
+	// Generate the number of names we were asked for
 	n := 1
 	for n <= number {
 		fmt.Println(getName(perms))
 		n++
 	}
-
 }
 
+// get an actual name from the list of permuations
 func getName(perms permutations) string {
 	var name []string
-
 	for t := range perms {
 		thing := perms[t][ran(len(perms[t]))]
 		name = append(name, thing)
-
 	}
-	return fmt.Sprintf(strings.Join(name, "-"))
-
+	return fmt.Sprintf(strings.Join(name, separator))
 }
 
 // ran picks a random positive int from 0 to max
@@ -68,23 +84,17 @@ func ran(max int) int {
 	return r.Intn(max)
 }
 
-// readData pulls all the data out of the files and returns the permutation
-func readData() (permutations, error) {
-
+// readData reads in the data for the types requested (eg: colour,dog,etc.)
+func readData(types []string) (permutations, error) {
 	perms := make(map[string][]string)
+	for _, asset := range types {
 
-	for _, asset := range data.AssetNames() {
-		_, file := filepath.Split(asset)
-		fileName := file[0 : len(file)-4] // Hacky, rip off .txt
-
-		data, err := readLines(asset)
+		data, err := readLines(fmt.Sprintf("%s%s", asset, dataFileSuffix))
 		if err != nil {
 			return perms, err
 		}
-		perms[fileName] = data
-
+		perms[asset] = data
 	}
-	// perms = allData
 	return perms, nil
 }
 
